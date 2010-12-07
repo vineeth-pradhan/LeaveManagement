6d8c3bd08e41388b4ed575d6aab0e76d0384659f
Vineeth Pradhan <vpvineeth@idexcel.com>
2010-12-07 09:59:24 +0530

    Initial commit of leave management system.

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '9eda9d22a21677eb1f6fe2b020baa3586047ecac702a88607587397b71dd55c8884493e7a37680b93325098be0d2ba819a3de4104971e5d37c28a083946465f4';
