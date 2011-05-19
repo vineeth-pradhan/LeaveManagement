ac01a6b9952728dea2148ebb5ca717cbf7d0fb71
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2011-05-19 11:36:29 +0530

    Rails 3 modifications

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeaveManagement::Application.config.secret_token = '4ea1dbd3769e9552ab83f1693d2e9b391aea736d644460d6043dbec206b64d4db9a4caa2bf4ca4935c008f2a5fb710b359d25c251ed307bc56377eb36d9948e1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,61 +1,86 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :images
+LeaveManagement::Application.routes.draw do |map|
+  
+  resources :images
 
 
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.register '/register', :controller => 'employees', :action => 'create'
-  map.signup '/signup', :controller => 'employees', :action => 'new'
-  map.resources :employees do |employee|
-    employee.resources 'available_offs'#, :only => ['create', 'index', 'show', 'update']
-    employee.resources 'applied_offs', :collection => 'update_status'#, :only => ['new','create','update']
-    employee.resource 'image' 
+  match "/logout" => "sessions#destroy"
+  
+#  map.login '/login', :controller => 'sessions', :action => 'new'
+   match "/login" => "sessions#new"
+  
+#  map.register '/register', :controller => 'employees', :action => 'create'
+   match "/register" => "employees#create"
+   
+#  map.signup '/signup', :controller => 'employees', :action => 'new'
+   match "/signup" => "employees#new", :as => :signup
+
+  map.resources :employees do
+    resources 'available_offs'#, :only => ['create', 'index', 'show', 'update']
+    resources 'applied_offs', :collection => 'update_status'#, :only => ['new','create','update']
+    resource 'image' 
   end
 
-  map.resources :designations
-  map.resources :leave_policies
-  map.resource :session
-  map.home '/',:controller => 'site', :action => 'site'
-
-  # The priority is based upon order of creation: first created -> highest priority.
+  resources :designations
+  resources :leave_policies
+  resource  :session
+#  map.home '/',:controller => 'site', :action => 'site'
+  root :to => "site#site"
+  
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
 
   # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+  #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
+  #   resources :products
 
   # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
 
   # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
   # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
   #   end
 
   # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
   #   end
 
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id(.:format)))'
 end
