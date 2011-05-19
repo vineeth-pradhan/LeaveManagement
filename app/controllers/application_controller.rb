9a51576d774f64ad75e92695cd0b41789ec009ac
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2011-05-19 10:50:02 +0530

    Temporary fix to get the Restful authentication to work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
+require File.join("#{RAILS_ROOT}/lib/authenticated_system.rb")
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   include AuthenticatedSystem
-
-  # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
 end
