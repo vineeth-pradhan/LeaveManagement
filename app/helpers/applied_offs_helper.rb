099afe75658f4bafc72e3a9db2ed473db9f01b58
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2011-05-23 14:48:33 +0530

    Changed the syntax from find(:all) to where && includes

--- app/helpers/applied_offs_helper.rb
+++ app/helpers/applied_offs_helper.rb
@@ -4,7 +4,8 @@ module AppliedOffsHelper
 #    e.available_offs.collect{|a|
 #      [a.leave_policy.policy_type.capitalize,a.id]
 #      }
-  available_offs=AvailableOff.all(:conditions =>["employee_id=?", e.id], :include => "leave_policy")
+  available_offs=AvailableOff.where(["employee_id = ?", e.id]).includes(:leave_policy)
+#  available_offs=AvailableOff.all(:conditions =>["employee_id=?", e.id], :include => "leave_policy")
   available_offs.collect{|i| i.leave_policy.policy_type.capitalize}
   end  
 end
