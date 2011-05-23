80953e049952574bcf34a02c43a263d0f0d32990
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2011-05-23 11:30:23 +0530

    Spec for new leave_policy form

--- spec/views/leave_policies/new.html.erb_spec.rb
+++ spec/views/leave_policies/new.html.erb_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
+
 describe "leave_policies/new.html.erb" do
-  it "renders a  form to create a new leave policy" do
-    assign(:leave_policy, double("LeavePolicy"))
+  it "renders a new form to create a new leave policy" do
+    assign(:leave_policy, mock_model("LeavePolicy").as_new_record.as_null_object)
     render
     rendered.should have_selector("form",
     :method => "post",
-    :action => leave_policy_path) do |form|
-      form.should have_selector("input", :type => "submit")
+    :action => leave_policies_path) do |form|
+      form.should have_selector("input",
+      :type => "text",
+      :name => "leave_policy[policy_type]")
+      form.should have_selector("input",
+      :type => "text",
+      :name => "leave_policy[no_of_days]")
+      form.should have_selector("input",
+      :type => "submit")
     end
   end
 end
