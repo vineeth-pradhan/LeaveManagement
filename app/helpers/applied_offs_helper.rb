e7977f975452234fc6a576ec377a14875d5abe63
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2011-05-23 14:37:32 +0530

    Eager loading for fething leave policies

--- app/helpers/applied_offs_helper.rb
+++ app/helpers/applied_offs_helper.rb
@@ -1,9 +1,10 @@
 module AppliedOffsHelper
   
   def fetch_available_leaves(e)
-    e.available_offs.collect{|a|
-      [a.leave_policy.policy_type.capitalize,a.id]
-      }
-  end
-  
+#    e.available_offs.collect{|a|
+#      [a.leave_policy.policy_type.capitalize,a.id]
+#      }
+  available_offs=AvailableOff.all(:conditions =>["employee_id=?", e.id], :include => "leave_policy")
+  available_offs.collect{|i| i.leave_policy.policy_type.capitalize}
+  end  
 end
