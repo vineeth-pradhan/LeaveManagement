0c26f4f1955a303409e2ca382b2ed283e8ee25c7
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2011-05-24 10:39:55 +0530

    Create method for the LeavePolicy Controller and its respective spec

--- app/controllers/leave_policies_controller.rb
+++ app/controllers/leave_policies_controller.rb
@@ -1,2 +1,11 @@
 class LeavePoliciesController < ApplicationController
+  def create
+    @leave_policy = LeavePolicy.new(params[:leave_policy])
+    if @leave_policy.save
+      flash[:notice]="The new Leave Policy was saved successfully."
+      redirect_to leave_policies_path
+    else
+      render :action => "new"
+    end
+  end
 end
