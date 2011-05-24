c44f65e09353bd0cd897e6d4c02d3e95deaec994
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2011-05-24 15:52:32 +0530

    Added validations for policy_type and no_of_days

--- app/models/leave_policy.rb
+++ app/models/leave_policy.rb
@@ -3,6 +3,4 @@ class LeavePolicy < ActiveRecord::Base
   has_many :applied_offs
   
   #TODO: validates_numericality_of :no_of_days
-  validates_presence_of :policy_type
-  validates_presence_of :no_of_days
 end
