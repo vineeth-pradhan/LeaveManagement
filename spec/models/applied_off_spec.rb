3dc75d546a210a63f4488e5201e98221d260dc2c
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2011-05-27 13:14:09 +0530

    Specs for the changed validations

--- spec/models/applied_off_spec.rb
+++ spec/models/applied_off_spec.rb
@@ -2,42 +2,102 @@ require 'spec_helper'
 include LoadInitialData
 
 describe AppliedOff do
-  context "AppliedOff Validations" do
-    before(:each) do
-      @applied_off=stub_model(AppliedOff,
-      :id => 1,
-      :available_off_id => 1,
-      :employee_id => 1,
-      :status => "pending",
-      :from_date => Time.now,
-      :to_date => Time.now+1.day)
+  
+  
+    context "validates_presence_of" do
+      before(:each) do      
+        LoadInitialData.load_all_designations
+        LoadInitialData.load_essential_employees
+        LoadInitialData.load_leave_policies
+        LoadInitialData.load_day_offs
+        @employee=Employee.first
+        @available_off=@employee.available_offs.first
+        @applied_off = stub_model(AppliedOff,
+        :id => 1,
+        :employee => @employee,
+        :available_off => @available_off,
+        :status => "pending",
+        :from_date => Time.now,
+        :to_date => Time.now+1.day)
+      end
+      # validate the applied_off object
+      it "is valid" do
+        @applied_off.should be_valid
+      end
+      
+      # validate_presence_of
+      it "is not valid without available_off_id" do
+        @applied_off.available_off_id = nil
+        @applied_off.should_not be_valid
+      end
+      
+      it "is not valid without employee_id" do
+        @applied_off.employee_id = nil
+        @applied_off.should_not be_valid
+      end
+      
+      it "is not valid without status" do
+        @applied_off.status = nil
+        @applied_off.should_not be_valid
+      end
+      
+      it "is not valid without from_date" do
+        @applied_off.from_date = nil
+        @applied_off.should_not be_valid
+      end
     end
-    it "is not valid without available_off_id" do
-      @applied_off.available_off_id = nil
-      @applied_off.should_not be_valid
+   
+   
+   
+   
+     context "Business logic validation" do
+       before(:each) do
+        LoadInitialData.load_all_designations
+        LoadInitialData.load_essential_employees
+        LoadInitialData.load_leave_policies
+        LoadInitialData.load_day_offs
+        @employee=Employee.first
+        @available_off=@employee.available_offs.first
+        @applied_off = stub_model(AppliedOff,
+        :id => 1,
+        :employee => @employee,
+        :available_off => @available_off,
+        :status => "pending",
+        :from_date => Time.now,
+        :to_date => Time.now+1.day)
+      end
+      
+      # validate from_date_not_to_be_greater_than_to_date
+      it "is not valid when from_date value is greater than to_date value" do
+        @applied_off.from_date = Time.now + 1.day.from_now
+        @applied_off.to_date = Time.now
+        @applied_off.should_not be_valid
+      end
+      
+      # When from_date and to_date are the same, it should be valid
+      it "is valid when both the from_date and to_date are the same" do
+        pending
+        @applied_off.from_date = Time.now
+        @applied_off.to_date = @applied_off.from_date
+        @applied_off.should be_valid
+      end
+      
+      # Check if the method get_days_in_number is working correctly
+      it "returns appropriate number of days of leave applied when to_date is subtracted from  from_date" do
+        @applied_off.from_date = Time.now
+        @applied_off.to_date = @applied_off.from_date
+        @applied_off.get_days_in_number(@applied_off.from_date, @applied_off.to_date).should == 1
+      end
+      
+      # Check if check_no_of_leaves works correctly
+      it "does not raise an error after checking the number of days of leave available is valid" do
+        pending
+        @applied_off.from_date = Time.now
+        @applied_off.to_date = @applied_off.from_date + 1.day.from_now
+      end 
     end
-    
-    it "is not valid without employee_id" do
-      @applied_off.employee_id = nil
-      @applied_off.should_not be_valid
-    end
-    
-    it "is not valid without status" do
-      @applied_off.status = nil
-      @applied_off.should_not be_valid
-    end
-    
-    it "is not valid without from_date" do
-      @applied_off.from_date = nil
-      @applied_off.should_not be_valid
-    end
-    
-    it "is valid when both the from_date and to_date are the same" do
-      @applied_off.from_date = Time.now
-      @applied_off.to_date = @applied_off.from_date
-      @applied_off.should be_valid
-    end
-  end  
+  
+  
   
   context "Fetching available leaves" do
     before(:each) do
